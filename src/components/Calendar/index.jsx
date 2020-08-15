49075ba78696497b0e0e2e4ee7316903a18da610
AlexNines <p39q10m4@gmail.com>
2020-08-15 15:03:32 +0400

    main functional of component done

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -3,28 +3,123 @@ import './index.css'
 
 export default class Calendar extends React.Component {
 
+
+    static defaultProps = {
+        date: new Date(),
+        years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+        monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        weekDayNames: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        onChange: Function.prototype
+    }
+
+    state = {
+        date: this.props.date,
+        currentDate: new Date(),
+        selectedDate: null
+    }
+
+    get year() {
+        return this.state.date.getFullYear()
+    }
+
+    get month() {
+        return this.state.date.getMonth()
+    }
+
+    handlePrevMonthButtonClick = () => {
+        const date = new Date(this.year, this.month - 1)
+        //console.log(date)
+        this.setState({ date })
+    }
+
+    handleNextMonthButtonClick = () => {
+        const date = new Date(this.year, this.month + 1)
+        //console.log(date)
+        this.setState({ date })
+    }
+
+    handleSelectChange = () => { 
+        const year = this.yearSelect.value
+        const month = this.monthSelect.value
+        const date = new Date( year, month )
+        this.setState({ date }) 
+        console.log(date)
+    }
+    handleDayClick = date => {
+        //console.log(date)
+        this.setState({ selectedDate: date })
+        this.props.onChange(date)
+    }
+
+
     render() {
+        const { years, monthNames, weekDayNames } = this.props
+
+        const monthData = [
+            [undefined, undefined, new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), new Date(), new Date(), new Date()],
+            [new Date(), new Date(), new Date(), new Date(), undefined, undefined, undefined],
+        ]
+
+
+
         return (
             <div className="calendar">
                 <header>
-                    <button>{'<'}</button>
+                    <button onClick={this.handlePrevMonthButtonClick}>{'<'}</button>
 
-                    <select></select>
+                    <select 
+                    ref={element => this.monthSelect = element}
+                    defaultValue={this.month}
+                    onChange={this.handleSelectChange}>
+                        {monthNames.map((name, index) =>
+                            <option key={name} value={index}>{name}</option>
+                        )}
+                    </select>
 
-                    <select></select>
+                    <select 
+                    ref={element => this.yearSelect = element}
+                    defaultValue={this.year}
+                    onChange={this.handleSelectChange}>
+                        {
+                            years.map(year =>
+                                <option key={year} value={year}>{year}</option>
+                            )
+                        }
+                    </select>
 
 
-                    <button>{'>'}</button>
+                    <button onClick={this.handleNextMonthButtonClick}>{'>'}</button>
                 </header>
 
                 <table>
                     <thead>
                         <tr>
-
+                            {
+                                weekDayNames.map(name =>
+                                    <th key={name}>{name}</th>
+                                )
+                            }
                         </tr>
                     </thead>
-                    <tbody>
 
+                    <tbody>
+                        {monthData.map((week, index) =>
+                            <tr key={index} className="week">
+                                {week.map((date, index) => date ?
+                                    <td
+                                        key={index}
+                                        className="day"
+                                        onClick={() => this.handleDayClick(date)}
+                                    >{date.getDate()}</td>
+                                    :
+                                    <td key={index} />
+
+                                )}
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
